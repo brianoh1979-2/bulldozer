6feb03e44fe16c673eca28046cb7a5cf70607191
Brian O'Halloran <brian_o_halloran@hotmail.com>
2020-08-19 19:56:10 +0100

    End to end test and putting in job to test code on K8S

---
+++ test-for-broken-code.sh
@@ -0,0 +1,11 @@
+currenttime=$(date +%H:%M)
+echo $currenttime
+if [[ "$currenttime" != "06:00" ]];
+then
+  echo "The code looks good - proceeding with deployment" 
+  exit 0
+else
+  echo "There is a problem with the code - aborting deployment" 
+  exit 125
+fi
+
